90c6b7f6c0df05819e2e0971ae37b4eded381d63
Pier107 <p.creanza@protonmail.com>
2024-04-07 19:51:02 +0200

    Code polishes

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -1,27 +1,6 @@
 import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "@/lib/utils"
 import { Badge } from "./badge";
 
-const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
-  {
-    variants: {
-      variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
-        secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-)
 interface TimelineEntryProps {
     date: string;
     title: string;
@@ -52,11 +31,6 @@ const TimelineEntry: React.FC<TimelineEntryProps> = ({ date, title, description,
     );
 };
 
-interface Event {
-    date: string;
-    content: string;
-}
-
 interface TimelineProps {
     children: React.ReactNode;
 }
@@ -71,4 +45,4 @@ const Timeline: React.FC<TimelineProps> = ({ children }) => {
         
     );
 };
-export {Timeline, TimelineEntry, badgeVariants }
+export {Timeline, TimelineEntry }
