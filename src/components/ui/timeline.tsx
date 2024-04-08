3fb9b99ae5ef6fd7e0eaa931b52fd8216c3bb69c
Pier107 <p.creanza@protonmail.com>
2024-04-08 15:36:25 +0200

    Convert timeline from table to ol list

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -11,23 +11,23 @@ interface TimelineEntryProps {
 
 const TimelineEntry: React.FC<TimelineEntryProps> = ({ date, title, description, badge, isDotted }) => {
     return (
-        <React.Fragment>
-            <tr className="h-full py-1"> 
-                <td className="align-top h-full"><h3 className="mr-5 text-right hidden md:block">{date}</h3></td>
-                <td className="align-top grid grid-cols-1 justify-items-center p-0 min-h-1 h-full relative">
-                    <div className="h-9 w-9  rounded-full bg-black dark:bg-white"/>
-                    <div className={`h-full absolute border-l-2 border-black borde dark:border-white ${isDotted && "border-dashed"}`} />
-                </td>
-                <td className="align-top max-w-2xl px-5 h-full">
+            <li className="h-full py-1 table-row"> 
+                <div className="table-cell align-top">
+                    <time className="mr-5 text-right hidden md:block scroll-m-20 text-2xl font-semibold tracking-tight">{date}</time>
+                </div>
+                <div className="flex justify-center p-0 min-h-1 h-full relative">
+                    <div className="h-9 w-9 rounded-full bg-black dark:bg-white"/>
+                    <div className={`absolute h-full border-l-2 border-black borde dark:border-white ${isDotted && "border-dashed"}`} />
+                </div>
+                <div className="table-cell align-top max-w-2xl px-5 h-full">
                     <h5 className="md:hidden mt-1">{date}</h5>
                     <h3>{title}</h3>
                     <p className={"muted"}>{description}</p>
                     <div className="mt-2 mb-8">
                         <Badge>{badge}</Badge>
                     </div>
-                </td>
-            </tr>
-        </React.Fragment>
+                </div>
+            </li>
     );
 };
 
@@ -38,9 +38,9 @@ interface TimelineProps {
 const Timeline: React.FC<TimelineProps> = ({ children }) => {
     return (
         <div className="flex justify-center mt-3">
-            <table className="">
+            <ol className="table">
                 {children}
-            </table>
+            </ol>
         </div>
         
     );
