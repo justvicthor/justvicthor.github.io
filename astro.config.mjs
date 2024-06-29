014917c2bf2703e6c6b72e118ff2ad3af013bd84
Vittorio Cozzoli <justvicthor@gmail.com>
2024-06-29 15:19:24 +0200

    Updated sites essentials

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://piervito.com",
+  site: "justvicthor.github.io",
   integrations: [
     react(),
     tailwind({
