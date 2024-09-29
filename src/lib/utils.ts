eb6032512f97dcd32192f0c0a4f591359ba67837
Vittorio Pio Remigio Cozzoli <justvicthor@gmail.com>
2024-09-29 13:30:50 +0200

    Bug fix attempt #2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const isValidBadgeVariant = (variant: string): variant is "default" | "secondary" | "destructive" | "outline" | "red" | "emerald" | "ochre" => {
+  return ["default", "secondary", "destructive", "outline", "red", "emerald", "ochre"].indexOf(variant) !== -1;
+};
+
+
